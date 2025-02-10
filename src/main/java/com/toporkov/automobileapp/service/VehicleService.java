df4bbd2ead331f00857c01a1fbc984bf3e0aa54c
egor <y.toporkov02@mail.ru>
2025-02-10 13:15:04 +0300

    Add Repository and Service for Vehicle

---
+++ src/main/java/com/toporkov/automobileapp/service/VehicleService.java
@@ -0,0 +1,23 @@
+package com.toporkov.automobileapp.service;
+
+import com.toporkov.automobileapp.model.Vehicle;
+import com.toporkov.automobileapp.repository.VehicleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    public List<Vehicle> findAll() {
+        return vehicleRepository.findAll();
+    }
+}
