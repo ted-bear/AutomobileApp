a01efc9f4b4a273e2cf351610277651df8fe6464
egor <y.toporkov02@mail.ru>
2025-02-10 12:37:25 +0300

    init

---
+++ src/main/java/com/toporkov/automobileapp/AutomobileAppApplication.java
@@ -0,0 +1,13 @@
+package com.toporkov.automobileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomobileAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toporkov/automobileapp/AutomobileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toporkov.automobileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
