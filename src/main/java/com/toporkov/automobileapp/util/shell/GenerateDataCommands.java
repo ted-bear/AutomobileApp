743fdb1287134f05550129313e1c2d26e9c0e6d8
egor <y.toporkov02@mail.ru>
2025-02-27 12:42:59 +0300

    Add data generator

--- src/main/java/com/toporkov/automobileapp/util/shell/GenerateDataCommands.java
+++ src/main/java/com/toporkov/automobileapp/util/shell/GenerateDataCommands.java
@@ -1,9 +1,13 @@
 package com.toporkov.automobileapp.util.shell;
 
 import com.toporkov.automobileapp.model.Condition;
+import com.toporkov.automobileapp.model.Driver;
+import com.toporkov.automobileapp.model.DriverAssignment;
 import com.toporkov.automobileapp.model.Enterprise;
 import com.toporkov.automobileapp.model.Vehicle;
 import com.toporkov.automobileapp.model.VehicleModel;
+import com.toporkov.automobileapp.repository.DriverAssignmentRepository;
+import com.toporkov.automobileapp.repository.DriverRepository;
 import com.toporkov.automobileapp.repository.EnterpriseRepository;
 import com.toporkov.automobileapp.repository.VehicleModelRepository;
 import com.toporkov.automobileapp.repository.VehicleRepository;
@@ -11,6 +15,7 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Random;
 
@@ -29,23 +34,41 @@ public class GenerateDataCommands {
             "Фиолетовый", "Розовый", "Коричневый", "Серый", "Белый"
     );
 
+    private final List<String> firstnames = List.of(
+            "Александр", "Елена", "Дмитрий", "Ольга", "Сергей",
+            "Анна", "Иван", "Мария", "Андрей", "Татьяна", "Николай",
+            "Ирина", "Алексей", "Екатерина"
+    );
+
+    private final List<String> lastnames = List.of(
+            "Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов",
+            "Васильев", "Попов", "Соколов", "Михайлов", "Федоров",
+            "Морозов", "Волков", "Алексеев", "Лебедев", "Семенов", "Егоров"
+    );
+
     private final Random random = new Random();
 
     private final VehicleRepository vehicleRepository;
+    private final DriverRepository driverRepository;
+    private final DriverAssignmentRepository driverAssignmentRepository;
     private final EnterpriseRepository enterpriseRepository;
     private final VehicleModelRepository vehicleModelRepository;
 
     public GenerateDataCommands(final VehicleRepository vehicleRepository,
+                                final DriverRepository driverRepository,
+                                final DriverAssignmentRepository driverAssignmentRepository,
                                 final EnterpriseRepository enterpriseRepository,
                                 final VehicleModelRepository vehicleModelRepository) {
         this.vehicleRepository = vehicleRepository;
+        this.driverRepository = driverRepository;
+        this.driverAssignmentRepository = driverAssignmentRepository;
         this.enterpriseRepository = enterpriseRepository;
         this.vehicleModelRepository = vehicleModelRepository;
     }
 
-    @ShellMethod(key = "generate-vehicles",
-            value = "Generate N random vehicles for specific enterprise.")
-    public String generateVehicles(
+    @ShellMethod(key = "generate-data",
+            value = "Generate N random vehicles and drivers for specific enterprise.")
+    public String generateVehiclesAndDrivers(
             @ShellOption(defaultValue = "1") Integer number,
             @ShellOption(defaultValue = "") List<Integer> enterpriseIds
     ) {
@@ -54,11 +77,21 @@ public class GenerateDataCommands {
             enterpriseRepository.findById(id)
                     .ifPresent(enterprise -> {
                         for (int i = 0; i < number; i++) {
-                            vehicleRepository.save(generateRandomVehicle(enterprise, vehicleModels));
+                            final Vehicle vehicle = generateRandomVehicle(enterprise, vehicleModels);
+                            final Driver driver = generateRandomDriver(enterprise);
+                            final DriverAssignment driverAssignment = new DriverAssignment(
+                                    driver,
+                                    vehicle,
+                                    getRandomBooleanWithFactor(0.1)
+                            );
+
+                            vehicleRepository.save(vehicle);
+                            driverRepository.save(driver);
+                            driverAssignmentRepository.save(driverAssignment);
                         }
                     });
         }
-        return "All vehicles:  " + vehicleRepository.findAll();
+        return "Generation is successful";
     }
 
     private Vehicle generateRandomVehicle(Enterprise enterprise, List<VehicleModel> models) {
@@ -76,9 +109,24 @@ public class GenerateDataCommands {
         return vehicle;
     }
 
+    private Driver generateRandomDriver(Enterprise enterprise) {
+        final Driver driver = new Driver();
+        driver.setEnterprise(enterprise);
+        driver.setFirstname(firstnames.get(random.nextInt(0, firstnames.size())));
+        driver.setLastname(lastnames.get(random.nextInt(0, lastnames.size())));
+        driver.setSalary(new BigDecimal(random.nextLong(0, 100_000)));
+        driver.setDrivingExperience(random.nextInt(0, 30));
+
+        return driver;
+    }
+
 
     private Condition getRandomCondition() {
         return random.nextBoolean() ? Condition.NEW : Condition.USED;
+    }
+
+    private boolean getRandomBooleanWithFactor(double probability) {
+        return random.nextDouble() < probability;
     }
 
 }
