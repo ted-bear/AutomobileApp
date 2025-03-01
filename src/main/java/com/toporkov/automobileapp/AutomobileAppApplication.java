10a1b7994a83e51d45fa7d389f89ac0132fc9fde
egor <y.toporkov02@mail.ru>
2025-03-01 14:17:47 +0300

    Add annotation for page with DTO

--- src/main/java/com/toporkov/automobileapp/AutomobileAppApplication.java
+++ src/main/java/com/toporkov/automobileapp/AutomobileAppApplication.java
@@ -3,12 +3,16 @@ package com.toporkov.automobileapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.shell.command.annotation.CommandScan;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @CommandScan
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class AutomobileAppApplication {
 
     public static void main(String[] args) {
