c515a4b3ef4ecb4cae0201698a468630643fa368
Egorka <trololo221@bk.ru>
2025-03-30 17:15:47 +0300

    Add method in VehicleCoordinateService to create a new Coordinate

--- src/main/java/com/toporkov/automobileapp/service/VehicleCoordinateService.java
+++ src/main/java/com/toporkov/automobileapp/service/VehicleCoordinateService.java
@@ -2,6 +2,8 @@ package com.toporkov.automobileapp.service;
 
 import com.toporkov.automobileapp.model.VehicleCoordinate;
 import com.toporkov.automobileapp.repository.VehicleCoordinateRepository;
+import com.toporkov.automobileapp.repository.VehicleRepository;
+import com.toporkov.automobileapp.web.dto.domain.coordinate.CreateCoordinateDTO;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
@@ -17,11 +19,14 @@ public class VehicleCoordinateService {
 
     private final GeometryFactory geometryFactory;
     private final VehicleCoordinateRepository vehicleCoordinateRepository;
+    private final VehicleRepository vehicleRepository;
 
     public VehicleCoordinateService(final GeometryFactory geometryFactory,
-                                    final VehicleCoordinateRepository vehicleCoordinateRepository) {
+                                    final VehicleCoordinateRepository vehicleCoordinateRepository,
+                                    final VehicleRepository vehicleRepository) {
         this.geometryFactory = geometryFactory;
         this.vehicleCoordinateRepository = vehicleCoordinateRepository;
+        this.vehicleRepository = vehicleRepository;
     }
 
     public List<VehicleCoordinate> findAllByTime(final int vehicleId,
@@ -30,6 +35,32 @@ public class VehicleCoordinateService {
 
         return vehicleCoordinateRepository
                 .findAllByVehicleIdAndCreateAtBetween(vehicleId, startTime, stopTime);
+    }
+
+    @Transactional
+    public void saveCoordinate(final CreateCoordinateDTO createCoordinateDTO) {
+        var vehicleCoordinate = mapDtoToEntity(createCoordinateDTO);
+        vehicleCoordinateRepository.save(vehicleCoordinate);
+    }
+
+    private VehicleCoordinate mapDtoToEntity(CreateCoordinateDTO createCoordinateDTO) {
+        var vehicleCoordinate = new VehicleCoordinate();
+
+        vehicleCoordinate.setCreateAt(Instant.now());
+        vehicleCoordinate.setLatitude(createCoordinateDTO.getLatitude());
+        vehicleCoordinate.setLongitude(createCoordinateDTO.getLongitude());
+        vehicleCoordinate.setPosition(
+                getPoint(
+                        createCoordinateDTO.getLatitude(),
+                        createCoordinateDTO.getLongitude()
+                )
+        );
+        vehicleCoordinate.setVehicle(vehicleRepository
+                .findById(createCoordinateDTO.getVehicleId())
+                .orElseThrow(RuntimeException::new)
+        );
+
+        return vehicleCoordinate;
     }
 
     private Point getPoint(Double latitude, Double longitude) {
---
+++ src/main/java/com/toporkov/automobileapp/web/dto/domain/coordinate/CreateCoordinateDTO.java
@@ -0,0 +1,59 @@
+package com.toporkov.automobileapp.web.dto.domain.coordinate;
+
+import java.time.Instant;
+
+public class CreateCoordinateDTO {
+
+    private Instant createAt;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private Integer vehicleId;
+
+    public CreateCoordinateDTO() {
+    }
+
+    public CreateCoordinateDTO(Instant createAt,
+                               Double latitude,
+                               Double longitude,
+                               Integer vehicleId) {
+        this.createAt = createAt;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.vehicleId = vehicleId;
+    }
+
+    public Instant getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Instant createAt) {
+        this.createAt = createAt;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(Integer vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+}
